5ad296f2dc6c81d9440eba2bc95caa358cc5b7d6
Adam Wead <awead@users.noreply.github.com>
2023-11-10 09:13:57 -0500

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-//! Records a WAV file (roughly 3 seconds long) using the default input device and config.
+//! Feeds back the input stream directly into the output stream.
 //!
-//! The input data is recorded to "$CARGO_MANIFEST_DIR/recorded.wav".
+//! Assumes that the input and output devices can use the same stream configuration and that they
+//! support the f32 sample format.
+//!
+//! Uses a delay of `LATENCY_MS` milliseconds in case the default input and output streams are not
+//! precisely synchronised.
 
 use clap::Parser;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{FromSample, Sample};
-use std::fs::File;
-use std::io::BufWriter;
-use std::sync::{Arc, Mutex};
+use ringbuf::HeapRb;
 
 #[derive(Parser, Debug)]
-#[command(version, about = "CPAL record_wav example", long_about = None)]
+#[command(version, about = "CPAL feedback example", long_about = None)]
 struct Opt {
-    /// The audio device to use
-    #[arg(short, long, default_value_t = String::from("default"))]
-    device: String,
+    /// The input audio device to use
+    #[arg(short, long, value_name = "IN", default_value_t = String::from("default"))]
+    input_device: String,
+
+    /// The output audio device to use
+    #[arg(short, long, value_name = "OUT", default_value_t = String::from("default"))]
+    output_device: String,
+
+    /// Specify the delay between input and output
+    #[arg(short, long, value_name = "DELAY_MS", default_value_t = 150.0)]
+    latency: f32,
 
     /// Use the JACK host
     #[cfg(all(
@@ -22,7 +31,8 @@ struct Opt {
             target_os = "linux",
             target_os = "dragonfly",
             target_os = "freebsd",
-            target_os = "netbsd"
+            target_os = "netbsd",
+            target_os = "macos"
         ),
         feature = "jack"
     ))]
@@ -31,22 +41,25 @@ struct Opt {
     jack: bool,
 }
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() -> anyhow::Result<()> {
     let opt = Opt::parse();
 
+    println!("jack is....");
     // Conditionally compile with jack if the feature is specified.
     #[cfg(all(
         any(
             target_os = "linux",
             target_os = "dragonfly",
             target_os = "freebsd",
-            target_os = "netbsd"
+            target_os = "netbsd",
+            target_os = "macos"
         ),
         feature = "jack"
     ))]
     // Manually check for flags. Can be passed through cargo with -- e.g.
     // cargo run --release --example beep --features jack -- --jack
     let host = if opt.jack {
+        println!("here?");
         cpal::host_from_id(cpal::available_hosts()
             .into_iter()
             .find(|id| *id == cpal::HostId::Jack)
@@ -62,117 +75,105 @@ fn main() -> Result<(), anyhow::Error> {
             target_os = "linux",
             target_os = "dragonfly",
             target_os = "freebsd",
-            target_os = "netbsd"
+            target_os = "netbsd",
+            target_os = "macos"
         )),
         not(feature = "jack")
     ))]
     let host = cpal::default_host();
 
-    // Set up the input device and stream with the default input config.
-    let device = if opt.device == "default" {
+    // Find devices.
+    let input_device = if opt.input_device == "default" {
         host.default_input_device()
     } else {
         host.input_devices()?
-            .find(|x| x.name().map(|y| y == opt.device).unwrap_or(false))
+            .find(|x| x.name().map(|y| y == opt.input_device).unwrap_or(false))
     }
     .expect("failed to find input device");
 
-    println!("Input device: {}", device.name()?);
+    let output_device = if opt.output_device == "default" {
+        host.default_output_device()
+    } else {
+        host.output_devices()?
+            .find(|x| x.name().map(|y| y == opt.output_device).unwrap_or(false))
+    }
+    .expect("failed to find output device");
 
-    let config = device
-        .default_input_config()
-        .expect("Failed to get default input config");
-    println!("Default input config: {:?}", config);
+    println!("Using input device: \"{}\"", input_device.name()?);
+    println!("Using output device: \"{}\"", output_device.name()?);
 
-    // The WAV file we're recording to.
-    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
-    let spec = wav_spec_from_config(&config);
-    let writer = hound::WavWriter::create(PATH, spec)?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
+    // We'll try and use the same configuration between streams to keep it simple.
+    let config: cpal::StreamConfig = input_device.default_input_config()?.into();
 
-    // A flag to indicate that recording is in progress.
-    println!("Begin recording...");
+    // Create a delay in case the input and output devices aren't synced.
+    let latency_frames = (opt.latency / 1_000.0) * config.sample_rate.0 as f32;
+    let latency_samples = latency_frames as usize * config.channels as usize;
 
-    // Run the input stream on a separate thread.
-    let writer_2 = writer.clone();
+    // The buffer to share samples
+    let ring = HeapRb::<f32>::new(latency_samples * 2);
+    let (mut producer, mut consumer) = ring.split();
 
-    let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
-    };
+    // Fill the samples with 0.0 equal to the length of the delay.
+    for _ in 0..latency_samples {
+        // The ring buffer has twice as much space as necessary to add latency here,
+        // so this should never fail
+        producer.push(0.0).unwrap();
+    }
 
-    let stream = match config.sample_format() {
-        cpal::SampleFormat::I8 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i8, i8>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::I16 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::I32 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i32, i32>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::F32 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        sample_format => {
-            return Err(anyhow::Error::msg(format!(
-                "Unsupported sample format '{sample_format}'"
-            )))
+    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        let mut output_fell_behind = false;
+        for &sample in data {
+            if producer.push(sample).is_err() {
+                output_fell_behind = true;
+            }
+        }
+        if output_fell_behind {
+            eprintln!("output stream fell behind: try increasing latency");
         }
     };
 
-    stream.play()?;
+    let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        let mut input_fell_behind = false;
+        for sample in data {
+            *sample = match consumer.pop() {
+                Some(s) => s,
+                None => {
+                    input_fell_behind = true;
+                    0.0
+                }
+            };
+        }
+        if input_fell_behind {
+            eprintln!("input stream fell behind: try increasing latency");
+        }
+    };
 
-    // Record 30s clip
-    std::thread::sleep(std::time::Duration::from_secs(30));
-    drop(stream);
-    writer.lock().unwrap().take().unwrap().finalize()?;
-    println!("Recording {} complete!", PATH);
+    // Build streams.
+    println!(
+        "Attempting to build both streams with f32 samples and `{:?}`.",
+        config
+    );
+    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn, None)?;
+    let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn, None)?;
+    println!("Successfully built streams.");
+
+    // Play the streams.
+    println!(
+        "Starting the input and output streams with `{}` milliseconds of latency.",
+        opt.latency
+    );
+    input_stream.play()?;
+    output_stream.play()?;
+
+    // Run for a given amount of time
+    std::thread::sleep(std::time::Duration::from_secs(3000));
+    drop(input_stream);
+    drop(output_stream);
+    println!("Done!");
     Ok(())
 }
 
-fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
-    if format.is_float() {
-        hound::SampleFormat::Float
-    } else {
-        hound::SampleFormat::Int
-    }
-}
-
-fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec {
-    hound::WavSpec {
-        channels: config.channels() as _,
-        sample_rate: config.sample_rate().0 as _,
-        bits_per_sample: (config.sample_format().sample_size() * 8) as _,
-        sample_format: sample_format(config.sample_format()),
-    }
-}
-
-type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
-
-fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
-where
-    T: Sample,
-    U: Sample + hound::Sample + FromSample<T>,
-{
-    if let Ok(mut guard) = writer.try_lock() {
-        if let Some(writer) = guard.as_mut() {
-            for &sample in input.iter() {
-                let sample: U = U::from_sample(sample);
-                writer.write_sample(sample).ok();
-            }
-        }
-    }
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {}", err);
 }
 
